ccb2be643a4bf586c5e8abb54c7676b574f540d2
CarlosOctavioHA <A01662079@itesm.mx>
2021-09-08 15:29:39 -0500

    main

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,7 +1,19 @@
 def main():
-    #escribe tu código abajo de esta línea
-    pass
+    if x>y:
+        if x>z:
+            print("el número mayor es:", x)
+    if y>x:
+        if y>z:
+            print("el número mayor es:", y)
+    if z>x:
+        if z>y:
+            print("el número mayor es:", z)
+    
 
-
-if __name__=='__main__':
-    main()
+print("Ingresa el primer número:")
+x=int(input())
+print("Ingresa el segundo número:")
+y=int(input())
+print("Ingresa el tercero número:")
+z=int(input())
+main()
--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,7 +1,33 @@
 def main():
-    # Escribe el código adecuado para completar el programa
-    pass
+    if x>y>z:
+        print(z)
+        print(y)
+        print(x)
+    if y>x>z:
+        print(z)
+        print(x)
+        print(y)
+    if z>x>y:
+        print(y)
+        print(x)
+        print(z)
+    if x>z>y:
+        print(y)
+        print(z)
+        print(x)
+    if y>z>x:
+        print(x)
+        print(z)
+        print(y)
+    if z>y>x:
+        print(x)
+        print(y)
+        print(z)
 
-
-if __name__=='__main__':
-    main()
+print("Ingresa el primer número:")
+x=int(input())
+print("Ingresa el segundo número:")
+y=int(input())
+print("Ingresa el tercero número:")
+z=int(input())
+main()
